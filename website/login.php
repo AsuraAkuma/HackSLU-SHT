f0f03e2973a66783a5b4edf7a235f5e46c4822c5
ImJustHenry <65806328+ImJustHenry@users.noreply.github.com>
2025-03-23 12:04:37 -0500

    test

--- website/login.php
+++ website/login.php
@@ -14,7 +14,7 @@ function getConnection()
 {
     $servername = "localhost";
     $username = "root";
-    $password = "h6wZFJGzfCud7F?f";
+    $password = "root";
     $dbname = "sht";
 
     $conn = new mysqli($servername, $username, $password, $dbname);
